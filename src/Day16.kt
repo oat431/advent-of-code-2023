8debe9038f5c77f31ba9589fcb6e584daecd54cd
oat431 <oat431@gmail.com>
2023-12-16 13:03:54 +0700

    🔨 add: day 16 template

---
+++ src/Day16.kt
@@ -0,0 +1,13 @@
+fun main() {
+    fun part1() : Int {
+        return 0
+    }
+
+    fun part2() : Int {
+        return 0
+    }
+
+    val input = readInput("input_day_16")
+    part1().println()
+    part2().println()
+}
